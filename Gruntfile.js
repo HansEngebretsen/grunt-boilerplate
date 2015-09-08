f51fdb69fb3146e36dd804215b1442aa0f365c2e
Engebretsen, Hans <Hans.Engebretsen@garmin.com>
2015-09-08 09:24:38 -0500

    reformatted gruntfile to add includes, proper grunt functionality

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,14 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-
-
         concat: {
             // 2. Configuration for concatinating files goes here.
             dist: {
                    src: [
-                       'js/libs/*.js', // All JS in the libs folder
-                       'js/*.js'
+                       // 'js/libs/*.js',  All JS in the libs folder
+                       'js/vender/*.js',
+                       'js/global.js',
+                       'js/modules/*.js'
                    ],
                    dest: 'js/build/production.js',
             }
@@ -24,21 +24,22 @@ module.exports = function(grunt) {
             }
           },
 
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'img/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'img/build/'
-                }]
-            }
-        },
+        // imagemin: {
+        //     dynamic: {
+        //         files: [{
+        //             expand: true,
+        //             cwd: '/img',
+        //             src: ['**/*.{png,jpg,gif}'],
+        //             dest: 'img/build'
+        //         }]
+        //     }
+        // },
 
         sass: {
             dist: {
                 options: {
-                    style: 'compressed'
+                    style: 'nested',
+                    lineNumbers: 'true'
                 },
                 files: {
                     'css/styles.css': 'sass/styles.scss'
@@ -47,22 +48,40 @@ module.exports = function(grunt) {
         },
          autoprefixer: {
             options: {
-              browsers: ['last 3 versions', '> 5%','ie 8', 'ie 7','ie 9']
+              browsers: ['last 13 versions', '> 5%','ie 8', 'ie 7','ie 9']
             },
             dist: {
                 files: {
-                    'style.css': 'style.css'
+                    'css/styles.css': 'css/styles.css'
                 }
             }
 
         },
+        // includes: {
+        //   files: {
+        //     src: ['src-html/*.html'], // Source files
+        //     dest: '../jb/', // Destination directory
+        //     flatten: true,
+        //     cwd: '.',
+        //     options: {
+        //       silent: true,
+        //       banner: '<!-- I am a banner <% includes.files.dest %> -->'
+        //     }
+        //   }
+        // },
 
        watch: {
-
-                  options: { livereload: true },
-
+            options: { livereload: true },
+            html: {
+              files: ['src-html/*.html', 'src-html/**/*.html'],
+              tasks: ['includes']
+            },
+            concat: {
+              files: ['js/vender/*.js', 'js/modules/*.js', 'js/global.js'],
+              tasks: ['concat']
+            },
             scripts: {
-              files: ['js/**/*.js'],
+              files: ['js/*.js'],
               tasks: ['concat', 'uglify'],
               options: {
                 spawn: false,
@@ -74,14 +93,14 @@ module.exports = function(grunt) {
               options: {
                 spawn: false,
               }
-            },
-            images: {
-              files: ['img/**/*.{png,jpg,gif}', 'img/*.{png,jpg,gif}'],
-              tasks: ['imagemin'],
-              options: {
-                spawn: false,
-              }
             }
+            // images: {
+            //   files: ['img/**/*.{png,jpg,gif}', 'img/*.{png,jpg,gif}'],
+            //   tasks: ['imagemin'],
+            //   options: {
+            //     spawn: false,
+            //   }
+            // }
 
         }
 
@@ -92,6 +111,7 @@ module.exports = function(grunt) {
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    // grunt.loadNpmTasks('grunt-includes');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-autoprefixer');
@@ -99,5 +119,5 @@ module.exports = function(grunt) {
 
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'autoprefixer']);
 };
