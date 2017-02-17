3ab1f3f39def9347cb76cc97cfa5fa3b98f32f01
Hans <hans.engebretsen@garmin.com>
2017-02-16 22:05:01 -0600

    Shiny new grunt file. Adds ES6 / Browserify / Babel support. Organizes and updates tasks.  Adds watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,123 +1,200 @@
+/*
+  ==========
+  Main Grunt Build Configuration file
+
+  This file is the main configuration object for the Grunt
+  task runner.
+
+  GruntJS: http://gruntjs.com/
+  ==========
+*/
+
 module.exports = function(grunt) {
+  /* ====================
+     Load the NPM tasks from the package.json automatically.
+     ==================== */
 
-    // 1. All configuration goes here
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  require('load-grunt-tasks')(grunt);
 
-        concat: {
-            // 2. Configuration for concatinating files goes here.
-            dist: {
-                   src: [
-                       // 'js/libs/*.js',  All JS in the libs folder
-                       'js/vender/*.js',
-                       'js/global.js',
-                       'js/modules/*.js'
-                   ],
-                   dest: 'js/build/production.js',
-            }
-          },
 
-         uglify: {
-            build: {
-                src: 'js/build/production.js',
-                dest: 'js/build/production.min.js'
-            }
-          },
+  grunt.initConfig({ // Start the Party
+    pkg: grunt.file.readJSON('package.json'), // where the package.json be
 
-        // imagemin: {
-        //     dynamic: {
-        //         files: [{
-        //             expand: true,
-        //             cwd: '/img',
-        //             src: ['**/*.{png,jpg,gif}'],
-        //             dest: 'img/build'
-        //         }]
-        //     }
-        // },
+    /*
+    Set options for various tasks within the config file.
+    These options can be overriden at the task level, but
+    are listed here to provide a DRY interface for common
+    file paths, etc.  Feel free to add commonly used options.
+    */
 
-        sass: {
-            dist: {
-                options: {
-                    style: 'nested',
-                    lineNumbers: 'true'
-                },
-                files: {
-                    'css/styles.css': 'sass/styles.scss'
-                }
-            }
+    options: {
+      src: {
+        dirname: '',
+        images: 'img',
+        sass: 'sass',
+        js: 'js'
+      },
+      output: {
+        dirname: '',
+        images: 'img',
+        css: 'css',
+        js: 'js/build'
+      }
+    },
+
+    /* --------------------
+        browserfiy task
+       --------------------  */
+    browserify: {
+      all: {
+        options: {
+          transform: [
+            ['babelify', {
+              presets: ['es2015', 'stage-0'],
+              "plugins": ["transform-decorators-legacy"]
+            }]
+          ]
         },
-         autoprefixer: {
-            options: {
-              browsers: ['last 13 versions', '> 5%','ie 8', 'ie 7','ie 9']
-            },
-            dist: {
-                files: {
-                    'css/styles.css': 'css/styles.css'
-                }
-            }
+        expand: true,
+        cwd: "<%= options.src.js %>/",
+        src: ["*.js", "!build/*", "!libs/*"],
+        dest: "<%= options.output.js %>/",
+        nonull: true
+      }
+    },
 
+    /* --------------------
+       concat task (just for libs)
+      --------------------  */
+    concat: {
+      // 2. Configuration for concatinating files goes here.
+      dist: {
+        src: ['<%= options.src.js %>/libs/*.js', '<%= options.output.js %>/main.js'], // everything in libs, with our babelified js
+        dest: '<%= options.output.js %>/production.js',
+      }
+    },
+
+    /* --------------------
+       Uglify task
+      --------------------  */
+    uglify: {
+      options: {
+        preserveComments: false
+      },
+      build: {
+        src: '<%= options.output.js %>/production.js',
+        dest: '<%= options.output.js %>/production.min.js'
+      }
+    },
+
+    /* --------------------
+       imagemin task
+      --------------------  */
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: '<%= options.src.images %>/',
+          src: ['*.{png,jpg,gif}'],
+          dest: '<%= options.output.images %>/'
+        }]
+      }
+    },
+
+    /* --------------------
+       sass task
+      --------------------  */
+    sass: {
+      dev: {
+        options: {
+          outputStyle: 'expanded',
+          sourceComments: 'true',
+          sourcemap: 'file'
         },
-        // includes: {
-        //   files: {
-        //     src: ['src-html/*.html'], // Source files
-        //     dest: '../jb/', // Destination directory
-        //     flatten: true,
-        //     cwd: '.',
-        //     options: {
-        //       silent: true,
-        //       banner: '<!-- I am a banner <% includes.files.dest %> -->'
-        //     }
-        //   }
-        // },
-
-       watch: {
-            options: { livereload: true },
-            html: {
-              files: ['src-html/*.html', 'src-html/**/*.html'],
-              tasks: ['includes']
-            },
-            concat: {
-              files: ['js/vender/*.js', 'js/modules/*.js', 'js/global.js'],
-              tasks: ['concat']
-            },
-            scripts: {
-              files: ['js/*.js'],
-              tasks: ['concat', 'uglify'],
-              options: {
-                spawn: false,
-              }
-            },
-            css: {
-              files: ['sass/**/*.scss'],
-              tasks: ['sass', 'autoprefixer'],
-              options: {
-                spawn: false,
-              }
-            }
-            // images: {
-            //   files: ['img/**/*.{png,jpg,gif}', 'img/*.{png,jpg,gif}'],
-            //   tasks: ['imagemin'],
-            //   options: {
-            //     spawn: false,
-            //   }
-            // }
-
+        files: {
+          '<%= options.output.css %>/styles.css': '<%= options.src.sass %>/styles.scss'
         }
+      },
+      prod: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          '<%= options.output.css %>/styles.min.css': '<%= options.src.sass %>/styles.scss'
+        }
+      }
+    },
+
+    /* --------------------
+       Autoprefix things for meh
+      --------------------  */
+    autoprefixer: {
+      options: {
+        browsers: ['last 13 versions', '> 5%', 'ie 8', 'ie 7', 'ie 9']
+      },
+      dist: {
+        files: {
+          '<%= options.output.css %>/styles.css': '<%= options.output.css %>/styles.css'
+        }
+      }
+
+    },
+
+    /* --------------------
+        Browsersync task
+       --------------------  */
+    browserSync: {
+      bsFiles: {
+        src: [
+          '<%= options.output.css %>/*.css',
+          '<%= options.src.js %>/*.js'
+        ]
+      },
+      options: {
+        watchTask: true,
+        server: {
+          baseDir: "<%= options.output.dirname %>"
+        }
+      }
+    },
+
+
+    /* --------------------
+        watch settigns
+       --------------------  */
+    watch: {
+      options: {
+        livereload: true
+      },
+      scripts: {
+        files: ['<%= options.src.js %>/**/**/*.js'],
+        tasks: ['browserify', 'concat'],
+        options: {
+          spawn: false,
+        }
+      },
+      css: {
+        files: ['<%= options.src.sass %>/**/*.scss'],
+        // tasks: ['sass', 'autoprefixer', 'browserSync'],
+        tasks: ['sass', 'autoprefixer', 'browserSync'],
+        options: {
+          spawn: false,
+        }
+      },
+      images: {
+        files: ['<%= options.src.images %>/**/*.{png,jpg,gif}', '<%= options.src.images %>/*.{png,jpg,gif}'],
+        tasks: ['imagemin'],
+        options: {
+          spawn: false,
+        }
+      }
+    }
+  });
 
 
 
-    });
+// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+grunt.registerTask('default', ['browserify', 'concat', 'uglify', 'sass', 'autoprefixer']);
+grunt.registerTask('watchs', ['browserSync', 'watch']);
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-includes');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['concat', 'uglify', 'sass', 'autoprefixer']);
 };
